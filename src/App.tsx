9568debb7615fe04c0efbcee02b1c116f757168f
Danim Kim <kdn725@gmail.com>
2022-06-22 22:17:51 +0900

    🎉 project initiated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>안녕하세요. 프론트엔드 개발자 김다님입니다.</h1>
+      <p>저의 포트폴리오 사이트에 오신 것을 환영합니다.</p>
     </div>
   );
 }
