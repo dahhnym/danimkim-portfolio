14fbd0b9cb7234a7fcbbe16a74d7aff0c159b406
Danim Kim <kdn725@gmail.com>
2022-07-07 01:11:06 +0900

    💄 update global style

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export const GlobalStyle = createGlobalStyle`
+  a, button, li, h1, p {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`
