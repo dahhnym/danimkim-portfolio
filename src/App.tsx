92de0d3bd13d25fdfa6c8d42a8b8e0cf35a243d4
Danim Kim <kdn725@gmail.com>
2022-07-07 12:53:24 +0900

    🚧 WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
-import "./App.css";
-import "./styles/reset.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Splash from "./pages/Splash";
-import Intro from "./components/Intro";
-import styled from "styled-components";
+import React from 'react';
+import './App.css';
+import './styles/reset.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Splash from './pages/Splash';
+import Intro from './components/Intro';
+import styled from 'styled-components';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -13,37 +15,35 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/* 추후에 path 이름 '/'로 변경 예정 */}
-          <Route path='/splash' element={<Splash />} />
+          <Route path="/splash" element={<Splash />} />
         </Routes>
       </BrowserRouter>
       <Header />
-      <main>
-        <Intro />
-        <section>
-          <h2>Skills</h2>
-        </section>
-        <section>
-          <h2>Project</h2>
-        </section>
-        <section>
-          <h2>FAQ</h2>
-          <ul>
-            <li>질문1</li>
-            <li>질문2</li>
-            <li>질문3</li>
-            <li>질문4</li>
-            <li>질문5</li>
-            <li>질문6</li>
-          </ul>
-        </section>
-        <section>
-          <h2>Contact</h2>
-        </section>
-        <Scroll>Scroll →</Scroll>
-      </main>
-      <footer>
-        <p>&copy; Copyright. All rights reserved. Danim Kim 2022 </p>
-      </footer>
+      <Wrapper>
+        <Main>
+          <Intro />
+          <Skills />
+          <Projects />
+          <section>
+            <h2>FAQ</h2>
+            <ul>
+              <li>질문1</li>
+              <li>질문2</li>
+              <li>질문3</li>
+              <li>질문4</li>
+              <li>질문5</li>
+              <li>질문6</li>
+            </ul>
+          </section>
+          <section>
+            <h2>Contact</h2>
+          </section>
+          <Scroll>Scroll →</Scroll>
+        </Main>
+        <footer>
+          <p>&copy; Copyright. All rights reserved. Danim Kim 2022 </p>
+        </footer>
+      </Wrapper>
     </Container>
   );
 }
@@ -51,9 +51,58 @@ function App() {
 export default App;
 
 const Container = styled.div`
-  background: radial-gradient(#0fc2c0, transparent);
-  /* background: radial-gradient(ellipse at top, #0fc2c0, transparent 50%),
-    radial-gradient(ellipse at bottom, #008f8c, transparent 60%); */
+  background: radial-gradient(closest-corner at 30% 40%, #d99c9c, transparent),
+    radial-gradient(closest-corner at 70% 60%, #d38484, transparent);
+  /* height: 100vh; */
+  /* animation: 10s infinite ease-in-out alternate bgColorRotation; */
+  /* @keyframes bgColorRotation {
+    0% {
+      background: radial-gradient(
+          closest-corner at 30% 40%,
+          #d99c9c,
+          transparent
+        ),
+        radial-gradient(closest-corner at 70% 60%, #d38484, transparent);
+    }
+    30% {
+      background: radial-gradient(
+          closest-corner at 50% 40%,
+          #d99c9c,
+          transparent
+        ),
+        radial-gradient(closest-corner at 60% 60%, #d38484, transparent);
+    }
+    60% {
+      background: radial-gradient(
+          closest-corner at 50% 60%,
+          #d38484,
+          transparent
+        ),
+        radial-gradient(closest-corner at 60% 40%, #d99c9c, transparent);
+    }
+    100% {
+      background: radial-gradient(
+          closest-corner at 30% 40%,
+          #d38484,
+          transparent
+        ),
+        radial-gradient(closest-corner at 40% 40%, #d99c9c, transparent);
+    }
+  } */
+`;
+
+const Wrapper = styled.div`
+  border: solid 1px red;
+`;
+
+const Main = styled.main`
+  @media screen and (min-width: 640px) {
+    padding-left: 80px;
+  }
+  @media screen and (min-width: 1024px) {
+    padding-left: 127px;
+    /* height: calc(100vh - 120px); */
+  }
 `;
 
 const Scroll = styled.p`
@@ -63,7 +112,7 @@ const Scroll = styled.p`
   bottom: 3rem;
   writing-mode: vertical-lr;
   &:hover {
-    color: #0fc2c0;
+    color: ${(props) => props.theme.babypink};
     transition: 0.2s;
     cursor: default;
     animation: 0.4s infinite ease-out alternate bounce;
