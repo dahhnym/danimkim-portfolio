99ac642d39deef6099c23cfe0582076252916f6d
Danim Kim <kdn725@gmail.com>
2022-07-07 12:49:52 +0900

    ♻️ generate heading component

---
+++ src/elements/Heading.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+interface Props {
+  children: string;
+}
+
+const Heading = ({ children }: Props) => {
+  return <H2>{children}</H2>;
+};
+
+export default Heading;
+
+const H2 = styled.h2`
+  font-size: 3rem;
+  opacity: 40%;
+  font-family: 'Nanum Myeongjo', serif;
+  font-weight: bold;
+`;
