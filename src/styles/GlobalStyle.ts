ab4ee6b3ce23f7f62382d516646b2ba702ca7566
dahhnym <danimkim.dev@gmail.com>
2022-07-07 17:19:49 +0900

    💄 modify font-size for responsive design

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -2,7 +2,14 @@ import { createGlobalStyle } from 'styled-components';
 
 
 export const GlobalStyle = createGlobalStyle`
-  a, button, li, h1, p {
-    font-family: 'Noto Sans KR', sans-serif;
-  }
+    html {
+        @media screen and (max-width: 640px) {
+            font-size: 14px;
+        }        
+        font-size: 16px;
+    }
+
+    a, button, li, h1, p {
+        font-family: 'Noto Sans KR', sans-serif;
+    }
 `
